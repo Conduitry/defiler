76987f83b54fadab23d281db6356e6cdb02369a9
Conduitry <git@fur.army>
2017-04-23 21:01:29 -0400

    build tweaks

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 export default {
-	entry: 'src/index.js',
-	external: ['events', 'fs', 'path'],
+	entry: './src/index.js',
+	external: name => /^[-a-z]+$/.test(name),
+	interop: false,
 	sourceMap: true,
-	targets: [{ dest: 'dist/index.cjs.js', format: 'cjs' }, { dest: 'dist/index.es.js', format: 'es' }],
+	targets: [{ dest: './dist/index.cjs.js', format: 'cjs' }, { dest: './dist/index.es.js', format: 'es' }],
 }
