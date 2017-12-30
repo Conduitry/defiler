402637b970d0827931d675211c7ae6f875d7340c
Conduitry <git@fur.army>
2017-12-29 20:56:15 -0500

    update deps again; update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,8 @@ export default {
 	input: './src/index.js',
 	external: name => /^[-a-z]+$/.test(name),
 	interop: false,
-	sourcemap: true,
 	output: [
-		{ file: './dist/index.cjs.js', format: 'cjs' },
-		{ file: './dist/index.es.js', format: 'es' },
+		{ file: './dist/index.cjs.js', format: 'cjs', sourcemap: true },
+		{ file: './dist/index.es.js', format: 'es', sourcemap: true },
 	],
 }
