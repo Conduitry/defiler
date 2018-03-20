d205b473e12fbf33990b2cc32af7e9343f65d71c
Conduitry <git@fur.army>
2018-03-19 23:04:56 -0400

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 export default {
 	input: './src/index.js',
 	external: name => /^[-a-z]+$/.test(name),
-	interop: false,
 	output: [
-		{ file: './dist/index.cjs.js', format: 'cjs', sourcemap: true },
-		{ file: './dist/index.es.js', format: 'es', sourcemap: true },
+		{ file: './dist/index.cjs.js', format: 'cjs', sourcemap: true, interop: false },
+		{ file: './dist/index.es.js', format: 'es', sourcemap: true, interop: false },
 	],
 }
