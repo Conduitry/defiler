7f31ad1d2ab74f0469c59eabd9bd58dcf6aa20e3
Conduitry <git@chor.date>
2018-11-13 06:03:13 -0500

    add typescript declaration file

---
+++ types/index.d.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+
+export class Defiler {
+    paths: Set<string>;
+    files: Map<string, File>;
+    constructor(...args: any[]);
+    exec(): Promise<void>;
+    get(path: string): Promise<File>;
+    get(paths: string[]): Promise<File[]>;
+    get(filter: Filter): Promise<File[]>;
+    add(file: FileData): void;
+    resolve(path: string): string;
+}
+
+export class File {
+	stats: fs.Stats;
+	path: string;
+	dir: string;
+	filename: string;
+	ext: string;
+	enc: string;
+	bytes: Buffer;
+	text: string;
+}
+
+interface Filter {
+    (path: string): boolean;
+}
+
+interface FileData {
+    path: string;
+    [propName: string]: any;
+}
