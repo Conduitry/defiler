c9f7dcdbbd18b793411df4154be228c931bd6a64
Conduitry <git@chor.date>
2022-12-12 10:35:17 -0500

    update to Rollup 3

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,21 @@ import cheap_ts from 'rollup-plugin-cheap-ts';
 
 export default {
 	input: './src/index',
-	external: name => /^[a-z]/.test(name),
+	external: (name) => /^[a-z]/.test(name),
 	plugins: [cheap_ts()],
 	output: [
-		{ file: './dist/index.cjs', format: 'cjs', sourcemap: true, interop: false, preferConst: true },
-		{ file: './dist/index.mjs', format: 'esm', sourcemap: true, preferConst: true },
+		{
+			file: './dist/index.cjs',
+			format: 'cjs',
+			sourcemap: true,
+			interop: 'esModule',
+			generatedCode: { constBindings: true },
+		},
+		{
+			file: './dist/index.mjs',
+			format: 'esm',
+			sourcemap: true,
+			generatedCode: { constBindings: true },
+		},
 	],
 };
